86cf16a1f365fc9fad6f7f8990a9ba2fde21158c
Shannon L Johnson-Martin <shannonljohnson-martin@Shannons-MBP.attlocal.net>
2021-03-23 22:31:12 -0400

    Add ghpages and scripts to package.jsonand image to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import banner from "./assets/img/banner.jpg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={banner} className="AppBanner" alt="geometric shapes" />
+        <p>Portfolio</p>
         <a
           className="App-link"
           href="https://reactjs.org"
